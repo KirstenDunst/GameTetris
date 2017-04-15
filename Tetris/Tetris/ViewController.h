efa094ccd2bd85097416cb46f9b23e95d7505fa4
KirstenDunst <cao_shixin@yahoo.com>
2017-04-15 11:37:07 +0800

    俄罗斯方块基本框架

---
+++ Tetris/Tetris/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tetris
+//
+//  Created by CSX on 2017/4/15.
+//  Copyright © 2017年 宗盛商业. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Tetris/Tetris/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Tetris
+//
+//  Created by CSX on 2017/4/15.
+//  Copyright © 2017年 宗盛商业. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
