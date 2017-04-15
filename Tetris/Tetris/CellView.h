ac47c70e4b1ef9193449a6abcd6274a50a18af96
KirstenDunst <cao_shixin@yahoo.com>
2017-04-15 18:04:07 +0800

    初始简易UI

---
+++ Tetris/Tetris/CellView.h
@@ -0,0 +1,15 @@
+//
+//  CellView.h
+//  Tetris
+//
+//  Created by CSX on 2017/4/15.
+//  Copyright © 2017年 宗盛商业. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CellView : UIView
+
+@property(nonatomic, assign)BOOL isSelect;
+
+@end
--- Tetris/Tetris/TetrisView.m
+++ Tetris/Tetris/TetrisView.m
@@ -7,6 +7,12 @@
 //
 
 #import "TetrisView.h"
+#import "CellView.h"
+
+typedef enum :NSInteger{
+    cellTag = 10,
+}Tags;
+
 
 #define cellWith  15           //定义每一个小的方块的宽度
 
@@ -14,10 +20,26 @@
 {
     UILabel *label;
 }
+//大数组
+@property (nonatomic, strong)NSMutableArray *dataArr;
+//小数组
+@property (nonatomic, strong)NSMutableArray *tempArr;
 @end
 
 @implementation TetrisView
 
+- (NSMutableArray *)tempArr{
+    if (!_tempArr) {
+        _tempArr = [NSMutableArray array];
+    }
+    return _tempArr;
+}
+- (NSMutableArray *)dataArr{
+    if (!_dataArr) {
+        _dataArr = [NSMutableArray array];
+    }
+    return _dataArr;
+}
 - (instancetype)initWithFrame:(CGRect)frame{
     self = [super initWithFrame:frame];
     if (self) {
@@ -40,13 +62,29 @@
     //15*25的方格
     UIView *gameView = [[UIView alloc]initWithFrame:CGRectMake(20, 150, cellWith*15, cellWith*25)];
     gameView.backgroundColor = [UIColor blackColor];
-    gameView.layer.borderWidth = 5;
+    gameView.layer.borderWidth = 3;
     gameView.layer.borderColor = [[UIColor whiteColor] CGColor];
     [self addSubview:gameView];
+    for (int i = 0; i<15*25; i++) {
+        CellView *bgView = [[CellView alloc]initWithFrame:CGRectMake(i%15*cellWith, i/15*cellWith, cellWith-0.5, cellWith-0.5)];
+        bgView.layer.cornerRadius = 3;
+        bgView.clipsToBounds = YES;
+        bgView.isSelect = NO;
+        [gameView addSubview:bgView];
+    }
+    
+    
     
     UIView *lastTypeView = [[UIView alloc]initWithFrame:CGRectMake(frame.size.width-cellWith*4, 150, cellWith*4, cellWith*4)];
     lastTypeView.backgroundColor = [UIColor blackColor];
     [self addSubview:lastTypeView];
+    for (int i = 0; i<4*4; i++) {
+        CellView *bgView = [[CellView alloc]initWithFrame:CGRectMake(i%4*cellWith, i/4*cellWith, cellWith-0.5, cellWith-0.5)];
+        bgView.layer.cornerRadius = 3;
+        bgView.clipsToBounds = YES;
+        bgView.isSelect = NO;
+        [lastTypeView addSubview:bgView];
+    }
     
     CGFloat buttonWidth = 70; //按钮的宽高
     
